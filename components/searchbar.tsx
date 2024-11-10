3dc535ef3f6a6b79f15b0902a98848dec40df8e1
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-10 10:37:35 +0530

    feat: crated a simple searchbar component

---
+++ components/searchbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export const SearchBar = () => {
+    const handleSubmit = () => { };
+    return (
+        <form
+            onClick={handleSubmit}
+            className="flex flex-wrap gap-4 mt-12"
+        >
+            <input
+                className="searchbar-input"
+                placeholder="Product Link"
+                type="text"
+            />
+        </form>
+    );
+};
