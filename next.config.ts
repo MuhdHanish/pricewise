e8836320207d924843fa750f5434600744d249d9
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-10 15:38:20 +0530

    chore: updated nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    domains: ['m.media-amazon.com']
+  }
 };
 
 export default nextConfig;
