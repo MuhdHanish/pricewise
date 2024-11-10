3eeccd7f63daa8a52a6792ca954f3f5d5676b857
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-10 15:55:02 +0530

    feat: created function for connect databsae using mongoose

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndSaveProduct(url: string) {
@@ -10,6 +11,7 @@ export async function scrapeAndSaveProduct(url: string) {
     try {
         const product = await scrapeAmazonProduct(url);
         if (!product) return;
+        connectToDB();
         return product;
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error?.message}`)
