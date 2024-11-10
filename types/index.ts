8d6f0034a978220cfc8d23b080f5d2b250b48b17
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-10 17:03:39 +0530

    chore: updated types

--- types/index.ts
+++ types/index.ts
@@ -16,7 +16,7 @@ export type Product = {
     title: string;
     currentPrice: number;
     originalPrice: number;
-    priceHistory: PriceHistoryItem[] | [];
+    priceHistory: PriceHistoryItem[];
     highestPrice: number;
     lowestPrice: number;
     averagePrice: number;
