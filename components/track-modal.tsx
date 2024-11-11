24c796486e2e8d6f26cd6e80405502b1ac4651f4
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-11 08:26:39 +0530

    feat: created a simple track-modal componet

---
+++ components/track-modal.tsx
@@ -0,0 +1,10 @@
+export const TrackModal = () => {
+    return (
+        <button
+            type="button"
+            className="btn"
+        >
+            Track
+        </button>
+    );
+};
