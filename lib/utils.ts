cd357571266bccef06a66186e69b3f0673254834
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-12 17:45:23 +0530

    feat: added formatDateTime func in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -128,3 +128,73 @@ export const formatNumber = (num: number = 0) => {
         maximumFractionDigits: 0,
     });
 };
+
+export const formatDateTime = (dateString: Date | string) => {
+    const dateTimeOptions: Intl.DateTimeFormatOptions = {
+        // weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+        month: "short", // abbreviated month name (e.g., 'Oct')
+        day: "numeric", // numeric day of the month (e.g., '25')
+        year: "numeric", // numeric year (e.g., '2023')
+        hour: "numeric", // numeric hour (e.g., '8')
+        minute: "numeric", // numeric minute (e.g., '30')
+        hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    };
+
+    const dateDayOptions: Intl.DateTimeFormatOptions = {
+        weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+        year: "numeric", // numeric year (e.g., '2023')
+        month: "2-digit", // abbreviated month name (e.g., 'Oct')
+        day: "2-digit", // numeric day of the month (e.g., '25')
+    };
+
+    const dateOptions: Intl.DateTimeFormatOptions = {
+        month: "short", // abbreviated month name (e.g., 'Oct')
+        year: "numeric", // numeric year (e.g., '2023')
+        day: "numeric", // numeric day of the month (e.g., '25')
+    };
+
+    const timeOptions: Intl.DateTimeFormatOptions = {
+        hour: "numeric", // numeric hour (e.g., '8')
+        minute: "numeric", // numeric minute (e.g., '30')
+        hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    };
+
+    const dateOfBirthOptions: Intl.DateTimeFormatOptions = {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric'
+    };
+
+    const formattedDateTime: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateTimeOptions
+    );
+
+    const formattedDateDay: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateDayOptions
+    );
+
+    const formattedDate: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateOptions
+    );
+
+    const formattedTime: string = new Date(dateString).toLocaleString(
+        "en-US",
+        timeOptions
+    );
+
+    const formatteDateOfBirth: string = new Date(dateString).toLocaleString(
+        "en-US",
+        dateOfBirthOptions
+    )
+
+    return {
+        dateTime: formattedDateTime,
+        dateDay: formattedDateDay,
+        dateOnly: formattedDate,
+        timeOnly: formattedTime,
+        dateOfBirth: formatteDateOfBirth
+    };
+};
