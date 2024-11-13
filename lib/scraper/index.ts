60aab0aad27b2d21e4f387880b54f814240c7dc1
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-13 11:10:23 +0530

    feat: added consle in scraper

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -87,6 +87,7 @@ export async function scrapeAmazonProduct(url: string) {
         };
         return data;
     } catch (error: any) {
+        console.error(error);
         throw new Error(`Failed to scrape product: ${error?.message}`);
     }
 }
