4f5fd099a89deeb941f7ac52109eeb435a4f62dd
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-15 07:31:01 +0530

    refactor: removed watch image from carousel

--- components/hero-carousel.tsx
+++ components/hero-carousel.tsx
@@ -6,20 +6,16 @@ import Image from "next/image";
 
 const heroImages = [
     {
-        imgUrl: `/assets/images/hero-1.svg`,
-        alt: `smart-watch`
-    },
-    {
-        imgUrl: `/assets/images/hero-2.svg`,
-        alt: `bag`
+        imgUrl: `/assets/images/hero-4.svg`,
+        alt: `air-fryer`
     },
     {
         imgUrl: `/assets/images/hero-3.svg`,
         alt: `lamp`
     },
     {
-        imgUrl: `/assets/images/hero-4.svg`,
-        alt: `air-fryer`
+        imgUrl: `/assets/images/hero-2.svg`,
+        alt: `bag`
     },
     {
         imgUrl: `/assets/images/hero-5.svg`,
